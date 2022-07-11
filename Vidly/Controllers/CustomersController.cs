1bd0fd5dde9f64ffedfefda52f9e9b2b7214ea82
CameronVincent <54481432+CameronVincent@users.noreply.github.com>
2022-07-11 16:01:25 -0400

    Updating to match vlaidation lecture code

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -36,6 +36,7 @@ namespace Vidly.Controllers
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
 
@@ -71,8 +72,20 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+
+                return View("CustomerForm", viewModel);
+            }
+
             if (customer.Id == 0)
                 _context.Customers.Add(customer);
             else
--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -9,11 +9,18 @@ namespace Vidly.Models
     public class MembershipType
     {
         public byte Id { get; set; }
+
         [Required]
         [StringLength(30)]
         public string Name { get; set; }
+
         public short SignUpFee { get; set; }
+
         public byte DurationInMonths { get; set; }
+
         public byte DiscountRate { get; set; }
+
+        public static readonly byte Unknown = 0;
+        public static readonly byte PayAsYouGo = 1;
     }
 }
